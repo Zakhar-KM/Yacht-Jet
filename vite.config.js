b5512221d746200ee9c6b58e12cfc499131ed1e1
Zakhar Kurta <zakhar.kurta@gmail.com>
2025-03-28 12:38:06 +0100

    Fix base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Yacht-Jet/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
